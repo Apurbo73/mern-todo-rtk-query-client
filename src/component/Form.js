090575028eb6816f379c06bd906ccefda8438f27
Apurbo73 <apurbodebnath895@gmail.com>
2023-07-21 03:06:39 +0600

    finally something is added to mongodb by rtk query

--- src/component/Form.js
+++ src/component/Form.js
@@ -1,10 +1,21 @@
-import React from "react";
-
+import React, { useState } from "react";
+import { useAddTodoMutation } from "../features/apiSlice";
 const Form = () => {
+    
+  const [todo, setTodo] = useState("");
+  const [addTodo, {isLoaading,isError,isSuccess,Error}]= useAddTodoMutation();
+
+  //handle Submit:
+  const handleSubmit=(e)=>{
+    e.preventDefault();
+    addTodo({todo})
+  }
   return (
     <div>
-      <form className="w-25 mx-auto mt-5">
-      <div className=" text-center mb-5"><h3>Mern Todo App</h3></div>
+      <form className="w-25 mx-auto mt-5" onSubmit={handleSubmit}>
+        <div className=" text-center mb-5">
+          <h3>Mern Todo App</h3>
+        </div>
         <div className="mb-3">
           <label htmlFor="exampleInputEmail1" className="form-label">
             Enter Todo Title
@@ -14,6 +25,9 @@ const Form = () => {
             className="form-control"
             id="exampleInputEmail1"
             aria-describedby="emailHelp"
+            onChange={e => {
+              setTodo(e.target.value);
+            }}
           />
         </div>
 
