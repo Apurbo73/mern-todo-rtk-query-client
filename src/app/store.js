e1124e37cbfa2e02f4f8e75d6ed92a2aeb456a14
Apurbo73 <apurbodebnath895@gmail.com>
2023-07-21 02:10:24 +0600

    store settup is done

--- src/app/store.js
+++ src/app/store.js
@@ -1,7 +1,10 @@
 import { configureStore } from '@reduxjs/toolkit';
+import { apiSlice } from '../features/apiSlice';
 
 export const store = configureStore({
   reducer: {
-    
+    [apiSlice.reducerPath]:apiSlice.reducer
   },
+  middleware: getDefaultMiddlewares =>
+    getDefaultMiddlewares().concat(apiSlice.middleware)
 });
