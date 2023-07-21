e929bfafbec2063aaa0869a0556e94d175457f7d
Apurbo73 <apurbodebnath895@gmail.com>
2023-07-21 14:39:18 +0600

    every todo got some design

--- src/App.js
+++ src/App.js
@@ -10,7 +10,11 @@ function App() {
   return (
     <div className="App">
       <Form />
+      <h6 className="mt-3 mb-2">All Your Todos</h6>
+      <div className="d-flex flex-wrap row">
       {allTodos && allTodos.map(t => <AlTodos t={t} />)}
+
+      </div>
     </div>
   );
 }
--- src/component/AlTodos.js
+++ src/component/AlTodos.js
@@ -1,11 +1,22 @@
-import React from 'react';
+import React from "react";
 
-const AlTodos = ({t}) => {
-    return (
-        <div>
-     <div className="btn btn-dark mb-2 mt-2">{t.data}</div>            
+const AlTodos = ({ t }) => {
+  return (
+    <div className="col-lg-3 col-md-4 ">
+      <div className="card-body m-3 bg-warning ">
+        <div className=" mb-2 mt-2 m-2 ">
+          <h6>
+            {t.data}
+          </h6>
         </div>
-    );
+        <br />
+        <div className="d-flex ">
+          <div className="btn btn-success m-2">Edit</div> <br />
+          <div className="btn btn-danger m-2">Delete</div>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default AlTodos;
